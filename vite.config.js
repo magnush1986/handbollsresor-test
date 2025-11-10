dbdaa007dfc3235dabc580da3f86fc0dcf3cfb68
magnush1986 <magnus.hemmen@gmail.com>
2025-11-10 22:42:59 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/handbollsresor-test/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
