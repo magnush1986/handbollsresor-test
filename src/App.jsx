8b42d72e14efc8a2a62e3a39c794720b83e9c258
magnush1986 <magnus.hemmen@gmail.com>
2025-11-10 22:59:52 +0100

    Remove Router component from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Handelser from './pages/Handelser';
 import Budget from './pages/Budget';
@@ -7,15 +7,13 @@ import Sasongsoversikt from './pages/Sasongsoversikt';
 
 export default function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Handelser />} />
-          <Route path="/budget" element={<Budget />} />
-          <Route path="/packlista" element={<Packlista />} />
-          <Route path="/sasongsoversikt" element={<Sasongsoversikt />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Handelser />} />
+        <Route path="/budget" element={<Budget />} />
+        <Route path="/packlista" element={<Packlista />} />
+        <Route path="/sasongsoversikt" element={<Sasongsoversikt />} />
+      </Routes>
+    </Layout>
   );
 }
